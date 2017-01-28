200cb039002836f9864521cfef8c23dd1cfe775b
Spectere99 <rflowers@saber98.com>
2017-01-28 16:45:06 -0500

    Added XERO Integration NuGet Package.  Added Generated SSL certificate and code to test it.

---
+++ XeroInvoiceIntegration/XeroIntegration.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Xero.Api.Core;
+using Xero.Api.Example.Applications.Private;
+using Xero.Api.Example.Applications.Public;
+using Xero.Api.Example.TokenStores;
+using Xero.Api.Infrastructure.OAuth;
+using Xero.Api.Serialization;
+
+namespace XeroInvoiceIntegration
+{
+    class XeroIntegration
+    {
+
+        public void CreateContact()
+        {
+            // Private Application Sample
+            X509Certificate2 cert = new X509Certificate2(@"E:\Projects\SouthPaw\Development\public_privatekey.pfx", "Spectere99");
+            var private_app_api = new XeroCoreApi("https://api.xero.com", new PrivateAuthenticator(cert),
+                new Consumer("ELKNE2VQNWH2RQQG3OV4KKOI4JMNGZ", "PKLBNCKBQSCOEXLCMG7IHYPPSSPQKO"), null,
+                new DefaultMapper(), new DefaultMapper());
+
+            var user = new ApiUser { Name = Environment.MachineName };
+
+            var org = private_app_api.Organisation;
+            
+            var contacts = private_app_api.Contacts.Find().ToList();
+
+            
+
+            // Public Application Sample
+            //var public_app_api = new XeroCoreApi("https://api.xero.com", new PublicAuthenticator("https://api.xero.com", "https://api.xero.com", "oob",
+            //    new MemoryTokenStore()),
+            //    new Consumer("F9WJSI6VC9TFJSOA6GPO6DFV8GFH1R", "KZZKTVEISETLPKXDAM3ZB7HQJTRQLM"), user,
+            //    new DefaultMapper(), new DefaultMapper());
+
+            //var public_contacts = public_app_api.Contacts.Find().ToList();
+
+        }
+    }
+}
